ca36ae4f4d263e3340399162eaa3dec9a8eadc80
Kaablo <pabloff1995@hotmail.com>
2022-06-15 13:24:27 +0200

    carga incial 2

--- src/recordadorajava/RecordadoraJava.java
+++ src/recordadorajava/RecordadoraJava.java
@@ -1,20 +1,96 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package recordadorajava;
+import java.util.Date;
+import java.util.Properties;
+import java.util.Scanner;
 
-/**
- *
- * @author pablo
- */
-public class RecordadoraJava {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.print("\n      -----------------------------------------------------------------------------------------------\n         Bienvenido/a, soy ReVi, tu recordadora virtual. ¿A qué recuerdos te gustaría acceder hoy?\n\n         PERSONAS (P), EVENTOS (E) o LISTAS (L)\n      -----------------------------------------------------------------------------------------------\n\n\n");
+
+        Scanner sca= new Scanner(System.in);
+        
+        String eleccion = sca.nextLine();
+
+
     }
+}
+
+class Memorias {
+
+   
+    private String nombre="";
+    private Date fecha=new Date();
+
+//contructores
+    public Memorias (){}
+
+    public Memorias (String nom, Date fecha){
+
+        this.nombre=nom;
+        this.fecha=fecha;
+    }
+
+//Metodos getter
+    public String dimeNombre(){
+
+        return nombre;
+    }
+
+    public Date dimeFecha(){
+
+        return fecha;
+    }
+
+//metodos setter
+    public void ponNombre(){
+        
+    }
+
+    public void ponFecha(){
+
+    }
+}
+
+class Persona extends Memorias {
+
+    int telefono=0;
+    String descripcion ="";
+
+    //constructor
+    public Persona (int tel, String desc){
+        super();
+        tel=telefono;
+        desc=descripcion;
+    }
+
+    //Metodos getter
+    public int dimeTelef(){
+
+        return telefono;
+    }
+
+    public String dimeDesc(){
+
+        return descripcion;
+    }
+
+    //metodos setter
+    public void ponTelef(){
+        
+    }
+
+    public void ponDesc(){
+
+    }
+}
+
+class Evento extends Memorias {
+
+    String Descripcion ="";
+
+}
+
+class Lista extends Memorias {
     
+    String [] elementos;
 }
