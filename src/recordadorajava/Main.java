6307b995eca0e93d6af8147e80fe36f416b2a744
Kaablo <pabloff1995@hotmail.com>
2022-06-17 13:22:58 +0200

    170622

--- src/recordadorajava/Main.java
+++ src/recordadorajava/Main.java
@@ -7,6 +7,8 @@ import recordadorajava.Persona;
 public class Main {
     public static void main(String[] args) throws Exception {
         
+       
+        
         System.out.print("\n\n      -----------------------------------------------------------------------------------------------\n         "
                 + "Bienvenido/a, soy ReVi, tu recordadora virtual. ¿A qué recuerdos te gustaría acceder hoy?\n\n         "
                 + "PERSONAS (P), EVENTOS (E) o LISTAS (L)\n"
@@ -35,7 +37,7 @@ public class Main {
                     System.out.print("La letra elegida es incorrecta");
                     try {
                     //Ponemos a "Dormir" el programa durante los ms que queremos
-                        Thread.sleep(3*1000);
+                        Thread.sleep(2*1000);
                     } catch (Exception e) {
                         System.out.println(e);
                     }
@@ -44,7 +46,7 @@ public class Main {
         }
     }
     static void ReInicio(){
-        System.out.print("\n\n\n\n      -----------------------------------------------------------------------------------------------\n         "
+        System.out.print("\n\n\n      -----------------------------------------------------------------------------------------------\n         "
                 + "¿A qué recuerdos te gustaría acceder hoy?\n\n         "
                 + "PERSONAS (P), EVENTOS (E) o LISTAS (L)\n"
                 + "      -----------------------------------------------------------------------------------------------\n\n\n");
@@ -57,7 +59,7 @@ public class Main {
     }
     
     static void RPersona(){
-        System.out.print("\n\n\n\n      -----------------------------------------------------------------------------------------------\n         "
+        System.out.print("\n\n\n      -----------------------------------------------------------------------------------------------\n         "
                 + "Acesso concedido a tus recuerdos sobre personas. ¿Quieres crear uno nuevo o consultarlo?\n\n         "
                 + "NUEVO (N) o CONSULTAR (C)\n"
                 + "      -----------------------------------------------------------------------------------------------\n\n\n");
@@ -68,23 +70,39 @@ public class Main {
         eleccion.toUpperCase();
         
         switch (eleccion){
-            case N:
-                System.out.print("\n\n\n\n      -----------------------------------------------------------------------------------------------\n         "
+            case "N":
+                System.out.print("\n\n\n      -----------------------------------------------------------------------------------------------\n         "
                 + "Creando recuerdo sobre una persona...\n ¿Como se llama esa persona?\n\n         "
                 + "Nombre:\n");
                
                 String nombre = sca.nextLine();
                 eleccion.toUpperCase();
                 
+                System.out.print("\n\n ¿Cuando nació? \n\n Cumpleaños(XX/XX/XXXX): ");                
+                String cumple =sca.nextLine();
+                
+                System.out.print("\n\n ¿Cuál es su telefono? \n\n Telefono: ");                               
+                Scanner sca2= new Scanner(System.in);
+                int tel = sca2.nextInt();
+                
+                System.out.print("\n\n ¿Otros datos? \n\n Datos: ");
+                String desc =sca.nextLine();
               
-                Persona n001 = new Persona();
+                Persona n001 = new Persona(nombre, cumple, tel, desc);
                 
+            default:
                 
-                Persona 
-                        
-                        
-            case C:
-                a
+                Persona n002 = new Persona("Pablo", "16/12/95",608181809, "Parece simpático");
+                System.out.print(n002.toString());
+                
+                try {
+                    //Ponemos a "Dormir" el programa durante los ms que queremos
+                        Thread.sleep(2*1000);
+                    } catch (Exception e) {
+                        System.out.println(e);
+                    }
+                    ReInicio();
         }
     }
 }
+        
--- src/recordadorajava/Persona.java
+++ src/recordadorajava/Persona.java
@@ -4,32 +4,71 @@
  */
 package recordadorajava;
 
+import java.util.Date;
+
 
 /**
  *
  * @author pablo
  */
-public class Persona extends Memorias{
+public class Persona {
 
+    private String nombre="";
+    private String fecha= "0/0/00";
+    private String descripcion ="";
     private int telefono=0;
 
     //constructor
     public Persona (){}
     
-    public Persona (int tel, String desc){
-        super();
-        tel=telefono;   
+    public Persona (String nom, String fecha, int tel, String desc){
+        
+        this.nombre=nom;
+        this.fecha=fecha;
+        this.descripcion=desc;
+        this.telefono=tel;   
     }
 
     //Metodos getter
+    public String dimeNombre(){
+        return nombre;
+    }
+
+    public String dimeFecha(){
+        return fecha;
+    }
+    public String dimeDesc(){
+
+        return descripcion;
+    }
     public int dimeTelef(){
         return telefono;
     }
 
     //metodos setter
+    public void ponNombre(){
+        
+    }
+
+    public void ponFecha(){
+
+    }
+    
+    public void ponDesc(){
+
+    }
+    
     public void ponTelef(){
         
     }
 
+    //Método toString
+    @Override
+    
+    public String toString(){
+        return "Nombre: "+ nombre+ "\nCumpleaños: "+fecha+"\nTeléfono: "+telefono+"\nOtros datos: "+descripcion;
+    }
+    
+    
 }
 
